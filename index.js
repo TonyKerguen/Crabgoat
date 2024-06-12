a744c155a495c74ff2d38375d07a304247bc5b07
TonyKerguen <tony.kerguen@etu.univ-orleans.fr>
2024-06-12 12:22:19 +0200

    r

--- index.js
+++ index.js
@@ -114,30 +114,62 @@ client.on('interactionCreate', async interaction => {
             interaction.reply('Une erreur s\'est produite lors de la récupération des données du joueur.');
         }
     }
-    if(interaction.commandName === 'buildchamp'){
-      const formattedChampionName = interaction.options.getString('nomchamp').toLowerCase().replace("'","").split(' ')[0]
+    // if(interaction.commandName === 'buildchamp'){
+    //   const formattedChampionName = interaction.options.getString('nomchamp').toLowerCase().replace("'","").split(' ')[0]
+    //   const urlstatschamp = `https://www.op.gg/champion/${formattedChampionName}/statistics`;
+    //   const { data } = await axios.get(urlstatschamp);
+
+    //   // Charger la page HTML dans cheerio pour le scraping
+    //   const $ = cheerio.load(data);
+
+    //   // Sélectionner les éléments contenant les informations de build
+    //   const builds = [];
+    //   $('.champion-overview__table--build td').each((i, element) => {
+    //       const buildItems = $(element).find('.champion-stats__list li img').map((i, el) => {
+    //           return $(el).attr('alt'); // Récupère le nom des objets
+    //       }).get();
+
+    //       if (buildItems.length > 0) {
+    //           builds.push(buildItems);
+    //       }
+    //   });
+
+    //   // return builds.length > 0 ? builds : null;
+    //   console.log(builds)
+    //   interaction.reply("builds");
+    // }
+    if (interaction.commandName === 'buildchamp') {
+      const formattedChampionName = interaction.options.getString('nomchamp').toLowerCase().replace("'", "").split(' ')[0];
       const urlstatschamp = `https://www.op.gg/champion/${formattedChampionName}/statistics`;
       const { data } = await axios.get(urlstatschamp);
-
+  
       // Charger la page HTML dans cheerio pour le scraping
       const $ = cheerio.load(data);
-
+  
+      // Affiche le HTML récupéré pour vérifier
+      console.log($.html());
+  
       // Sélectionner les éléments contenant les informations de build
       const builds = [];
       $('.champion-overview__table--build td').each((i, element) => {
+          // Affiche le HTML de chaque cellule pour vérification
+          console.log($(element).html());
+  
           const buildItems = $(element).find('.champion-stats__list li img').map((i, el) => {
-              return $(el).attr('alt'); // Récupère le nom des objets
+              const itemName = $(el).attr('alt');
+              console.log(itemName); // Affiche le nom de chaque objet trouvé
+              return itemName;
           }).get();
-
+  
           if (buildItems.length > 0) {
               builds.push(buildItems);
           }
       });
-
-      // return builds.length > 0 ? builds : null;
-      console.log(builds)
-      interaction.reply("builds");
-    }
+  
+      // Afficher les builds trouvés
+      console.log(builds);
+      interaction.reply("builds: " + (builds.length > 0 ? builds.map(build => build.join(', ')).join(' | ') : 'Aucun build trouvé'));
+  }
 });
 console.log("YOLO");
 
